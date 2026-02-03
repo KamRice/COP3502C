4290d9de215cadc2eae99cc2cff0e1f119176fa5
Kameron Rice <107422196+KamRice@users.noreply.github.com>
2026-02-03 09:16:29 -0500

    Draft 1, needs testing.

--- Project1/P1.py
+++ Project1/P1.py
@@ -1,15 +1,21 @@
 import p1_random as p1
 
+"""
+THINGS TO FIX:
+1. When player busts, add a loss for player. and a win for dealer.
+"""
+
 rng = p1.P1Random()
 
 """
 Global Variables
 """
-game_Count = 0
-games_Played = 1
+
 games_Won = 0
 games_Lost = 0
 games_Tie = 0
+games_Played = 0
+game_Count = 0
 
 card_Map = {
     1: {"name": "ACE", "value": 1},
@@ -36,9 +42,6 @@ def main():
     global game_Count
     game_Count += 1
 
-    # Dealer Hand Variables
-    dealers_hand = rng.next_int(11) + 16
-
     # Initial Player Hand
     player_current_card = card_Map[rng.next_int(13) + 1]
     player_score = player_current_card["value"]
@@ -51,16 +54,20 @@ def main():
     player_input_choice = validate_player_input()
 
     if player_input_choice == 1:
-        blackjack( player_score)
+        blackjack(player_score)
     elif player_input_choice == 2:
-        print("Option: ", player_input_choice)
+        hold_hand(player_score)
     elif player_input_choice == 3:
-        get_statistics()
+        get_statistics(player_score)
     else:
         print("Option: ", player_input_choice)
 
 
 def blackjack(score):
+    global games_Won
+    global games_Lost
+    global games_Played
+
     # Deal new player card.
     player_new_card = card_Map[rng.next_int(13) + 1]
     score += player_new_card["value"]
@@ -73,6 +80,8 @@ def blackjack(score):
 
     if score == 21:
         print("BLACKJACK! You win!\n")
+        games_Won += 1
+        games_Played += 1
         main()
     elif score >= 22:
         print("You exceeded 21! You lose.\n")
@@ -83,9 +92,9 @@ def blackjack(score):
     if player_input_choice == 1:
         blackjack(score)
     elif player_input_choice == 2:
-        print("Option: ", player_input_choice)
+        hold_hand(score)
     elif player_input_choice == 3:
-        get_statistics()
+        get_statistics(score)
     else:
         return
 
@@ -103,33 +112,61 @@ def validate_player_input():
         return player_input_choice
 
 
-def get_statistics():
+def get_statistics(score):
     global games_Won
     global games_Played
-
-    games_Won = 3
-    games_Played = 4
 
     print("\nNumber of Player Wins:", games_Won)
-    print("\nNumber of Dealer Wins:", games_Lost)
-    print("\nNumber of tie games:", games_Tie)
-    print("\nTotal # of games played is:", games_Played)
+    print("Number of Dealer Wins:", games_Lost)
+    print("Number of tie games:", games_Tie)
+    print("Total # of games played is:", games_Played)
 
-    win_percentage = (games_Won / games_Played)
+    if games_Played > 0:
+        win_percentage = (games_Won / games_Played) * 100
+    else:
+        win_percentage = 0
 
-    print("\nPercentage of Player wins:", round(win_percentage, 1))
+    print("\nPercentage of Player wins:", round(win_percentage, 1), "\n")
+
+    player_input_choice = validate_player_input()
+
+    if player_input_choice == 1:
+        blackjack(score)
+    elif player_input_choice == 2:
+        hold_hand(score)
+    elif player_input_choice == 3:
+        get_statistics(score)
+    else:
+        return
 
 
-def hold_hand():
-    global games_Played
+def hold_hand(score):
     global games_Won
     global games_Lost
+    global games_Tie
+    global games_Played
 
-    games_Played += 1
+    # Create dealer score.
+    dealer_score = rng.next_int(11) + 16
 
+    print("\nDealer's hand:", dealer_score)
+    print("Your hand is:", score)
 
-def give_new_card():
-    pass
+    if score > dealer_score:
+        print("\nYou win!")
+        games_Won += 1
+        games_Played += 1
+        main()
+    elif score < dealer_score:
+        print("\nDealer wins!\n")
+        games_Lost += 1
+        games_Played += 1
+        main()
+    else:
+        print("\nIt's a tie! No one wins!\n")
+        games_Tie += 1
+        games_Played += 1
+        main()
 
 
 """
