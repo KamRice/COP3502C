ce30996c4788d50160cf32a2dab1ed39bc1daba3
Kam Rice <Kameron.Rice@MyManatee.org>
2026-03-11 08:56:07 -0400

    lab8

---
+++ Lab8/Lab8.py
@@ -0,0 +1,43 @@
+def print_backwards(value):
+    if len(value) > 0:
+        print(value[len(value) - 1], end='', sep='')
+        print_backwards(value[0:len(value) - 1])
+    else:
+        print()
+
+
+def format_names(value):
+    out = []
+    for member in value:
+        if member[member.index(" ") - 1] != ",":
+            out.append(f"{member.split(' ')[1]}, {member.split(' ')[0]}")
+        else:
+            out.append(member)
+    return out
+
+
+def sum_a(data):
+    a_sum = 0
+    for dictionary in data:
+        for key, value in dictionary.items():
+            if key == 'a':
+                a_sum += int(value)
+    return a_sum
+
+
+def process_list(value):
+    out = []
+    for i in range(0, len(value), 2):
+        out.append(str(value[i]))
+    for i in range(1, len(value), 2):
+        out.append(value[i] * 10)
+    return out
+
+
+def group_by(values, func):
+    print(len(values))
+    if len(values) > 0:
+        {}.update(group_by(values[:-1], func))
+
+
+print(group_by([1, 2, 3, 4, 5, 6, 7, 8], lambda x: x % 2 == 0))
